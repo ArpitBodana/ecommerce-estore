61683f5c5d02dfb12054f0942d9b0949dfb0ae89
ArpitBodana <arpitbodana2@gmail.com>
2022-10-29 11:00:13 +0530

    introduce lazyness

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import PageNotFound from "./Pages/PageNotFound";
-import Product from "./Pages/Product";
 import Footer from "./Components/Footer";
 import { Button, Container } from "react-bootstrap";
-import Cart from "./Pages/Cart";
-import SignIn from "./Pages/SignIn";
 import { ToastContainer } from "react-toastify";
-import Shipping from "./Pages/Shipping";
-import SignUp from "./Pages/SignUp";
-import PaymentMethod from "./Pages/PaymentMethod";
-import PlaceOrder from "./Pages/PlaceOrder";
 import AppBar from "./Components/AppBar";
-import Order from "./Pages/Order";
-import OrderHistory from "./Pages/OrderHistory";
-import Profile from "./Pages/Profile";
 import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { toast } from "react-toastify";
 import axios from "axios";
-import Search from "./Pages/Search";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import AdminRoute from "./Components/AdminRoute";
-import Dashboard from "./Pages/Admin/Dashboard";
-import UserList from "./Pages/Admin/UserList";
-import EditUser from "./Pages/Admin/EditUser";
-import OrderList from "./Pages/Admin/OrderList";
-import ProductList from "./Pages/Admin/ProductList";
-import EditProduct from "./Pages/Admin/EditProduct";
-import AddProduct from "./Pages/Admin/AddProduct";
+import LoadingSpinner from "./Components/LoadingSpinner";
+
+// import Search from "./Pages/Search";
+// import Dashboard from "./Pages/Admin/Dashboard";
+// import UserList from "./Pages/Admin/UserList";
+// import EditUser from "./Pages/Admin/EditUser";
+// import OrderList from "./Pages/Admin/OrderList";
+// import ProductList from "./Pages/Admin/ProductList";
+// import EditProduct from "./Pages/Admin/EditProduct";
+// import AddProduct from "./Pages/Admin/AddProduct";
+// import Order from "./Pages/Order";
+// import OrderHistory from "./Pages/OrderHistory";
+// import Profile from "./Pages/Profile";
+// import Shipping from "./Pages/Shipping";
+// import SignUp from "./Pages/SignUp";
+// import PaymentMethod from "./Pages/PaymentMethod";
+// import PlaceOrder from "./Pages/PlaceOrder";
+// import Cart from "./Pages/Cart";
+// import SignIn from "./Pages/SignIn";
+// import PageNotFound from "./Pages/PageNotFound";
+// import Product from "./Pages/Product";
+// import Home from "./Pages/Home";
+
+const Search = lazy(() => import("./Pages/Search"));
+const Dashboard = lazy(() => import("./Pages/Admin/Dashboard"));
+const UserList = lazy(() => import("./Pages/Admin/UserList"));
+const EditUser = lazy(() => import("./Pages/Admin/EditUser"));
+const OrderList = lazy(() => import("./Pages/Admin/OrderList"));
+const ProductList = lazy(() => import("./Pages/Admin/ProductList"));
+const EditProduct = lazy(() => import("./Pages/Admin/EditProduct"));
+const AddProduct = lazy(() => import("./Pages/Admin/AddProduct"));
+const Order = lazy(() => import("./Pages/Order"));
+const OrderHistory = lazy(() => import("./Pages/OrderHistory"));
+const Profile = lazy(() => import("./Pages/Profile"));
+const Shipping = lazy(() => import("./Pages/Shipping"));
+const SignUp = lazy(() => import("./Pages/SignUp"));
+const PaymentMethod = lazy(() => import("./Pages/PaymentMethod"));
+const PlaceOrder = lazy(() => import("./Pages/PlaceOrder"));
+const Cart = lazy(() => import("./Pages/Cart"));
+const SignIn = lazy(() => import("./Pages/SignIn"));
+const PageNotFound = lazy(() => import("./Pages/PageNotFound"));
+const Product = lazy(() => import("./Pages/Product"));
+const Home = lazy(() => import("./Pages/Home"));
 
 function App() {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
@@ -100,120 +123,122 @@ function App() {
         </div>
         <main className="">
           <Container>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/product/:slug" element={<Product />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/signin" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route
-                path="/shipping"
-                element={
-                  <ProtectedRoute>
-                    <Shipping />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/payment"
-                element={
-                  <ProtectedRoute>
-                    <PaymentMethod />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/placeorder"
-                element={
-                  <ProtectedRoute>
-                    <PlaceOrder />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/order/:id/"
-                element={
-                  <ProtectedRoute>
-                    <Order />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/orderhistory"
-                element={
-                  <ProtectedRoute>
-                    <OrderHistory />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/profile"
-                element={
-                  <ProtectedRoute>
-                    <Profile />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/search" element={<Search />} />
-              {/*Admin Routes*/}
-              <Route
-                path="/admin/dashboard"
-                element={
-                  <AdminRoute>
-                    <Dashboard />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin/userlist"
-                element={
-                  <AdminRoute>
-                    <UserList />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin/edituser/:id"
-                element={
-                  <AdminRoute>
-                    <EditUser />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin/orderlist"
-                element={
-                  <AdminRoute>
-                    <OrderList />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin/productlist"
-                element={
-                  <AdminRoute>
-                    <ProductList />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin/editproduct/:id"
-                element={
-                  <AdminRoute>
-                    <EditProduct />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin/addproduct"
-                element={
-                  <AdminRoute>
-                    <AddProduct />
-                  </AdminRoute>
-                }
-              />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
+            <Suspense fallback={<LoadingSpinner />}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product/:slug" element={<Product />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route
+                  path="/shipping"
+                  element={
+                    <ProtectedRoute>
+                      <Shipping />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/payment"
+                  element={
+                    <ProtectedRoute>
+                      <PaymentMethod />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/placeorder"
+                  element={
+                    <ProtectedRoute>
+                      <PlaceOrder />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/order/:id/"
+                  element={
+                    <ProtectedRoute>
+                      <Order />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/orderhistory"
+                  element={
+                    <ProtectedRoute>
+                      <OrderHistory />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/profile"
+                  element={
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/search" element={<Search />} />
+                {/*Admin Routes*/}
+                <Route
+                  path="/admin/dashboard"
+                  element={
+                    <AdminRoute>
+                      <Dashboard />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/userlist"
+                  element={
+                    <AdminRoute>
+                      <UserList />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/edituser/:id"
+                  element={
+                    <AdminRoute>
+                      <EditUser />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/orderlist"
+                  element={
+                    <AdminRoute>
+                      <OrderList />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/productlist"
+                  element={
+                    <AdminRoute>
+                      <ProductList />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/editproduct/:id"
+                  element={
+                    <AdminRoute>
+                      <EditProduct />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/addproduct"
+                  element={
+                    <AdminRoute>
+                      <AddProduct />
+                    </AdminRoute>
+                  }
+                />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Suspense>
           </Container>
         </main>
         <Footer />
